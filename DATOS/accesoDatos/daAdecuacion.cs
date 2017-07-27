93b03c87d38390a3476f68a156252bcbfa3a89cd
Devninjap <perez_deyvi@hotmail.com>
2017-07-27 16:34:28 -0500

    siete

--- DATOS/accesoDatos/daAdecuacion.cs
+++ DATOS/accesoDatos/daAdecuacion.cs
@@ -81,49 +81,94 @@ namespace DATOS.accesoDatos
             }
         }
 
-		public object consultaReporte(int idAdecuacion)
-		{
-			try
-			{
-				using (adecuacionIscFiuplaEntities ctx = new adecuacionIscFiuplaEntities())
-				{
-					return (from estudiante in ctx.estudiante
-							from adecuacion in estudiante.adecuacion
-							from equivalencias in ctx.equivalenciaAsignatura
-							from detalles in adecuacion.detalleAdecuacion
-							//from detalles2 in equivalencias.detalleAdecuacion
-							where adecuacion.idAdecuacion == idAdecuacion
-							select new
-							{
-								estudiante.apellidosEstudiante,
-								estudiante.nombresEstudiante,
-								estudiante.codEstudiante,
-								estudiante.sede,
-								estudiante.modalidad,
-								estudiante.carrera,
-								adecuacion.codigoAdecuacion,
-								adecuacion.futAdecuacion,
-								adecuacion.fechaRecepcion,
-								adecuacion.periodo,
-								adecuacion.fechaEmision,
-								equivalencias.planAsignaturaB,
-								equivalencias.codAsignaturaB,
-								equivalencias.nomAsignaturaB,
-								equivalencias.codAsignaturaA,
-								equivalencias.creditosAsignaturaA,
-								equivalencias.cicloAsignaturaA,
-								equivalencias.nomAsignaturaA,
-								detalles.notaAsignaturaA,
-								detalles.notaAsignaturaB,
-								detalles.fechaActa,
-							}).ToList();
-				}
-			}
-			catch (Exception)
-			{
+		//public object consultaReporte(int idAdecuacion)
+		//{
+		//	try
+		//	{
+		//		using (adecuacionIscFiuplaEntities ctx = new adecuacionIscFiuplaEntities())
+		//		{
+		//			return (from estudiante in ctx.estudiante
+		//					from adecuacion in estudiante.adecuacion
+		//					from equivalencias in ctx.equivalenciaAsignatura
+		//					from detalles in adecuacion.detalleAdecuacion
+		//					//from detalles2 in equivalencias.detalleAdecuacion
+		//					where adecuacion.idAdecuacion == idAdecuacion
+		//					select new
+		//					{
+		//						estudiante.apellidosEstudiante,
+		//						estudiante.nombresEstudiante,
+		//						estudiante.codEstudiante,
+		//						estudiante.sede,
+		//						estudiante.modalidad,
+		//						estudiante.carrera,
+		//						adecuacion.codigoAdecuacion,
+		//						adecuacion.futAdecuacion,
+		//						adecuacion.fechaRecepcion,
+		//						adecuacion.periodo,
+		//						adecuacion.fechaEmision,
+		//						equivalencias.planAsignaturaB,
+		//						equivalencias.codAsignaturaB,
+		//						equivalencias.nomAsignaturaB,
+		//						equivalencias.codAsignaturaA,
+		//						equivalencias.creditosAsignaturaA,
+		//						equivalencias.cicloAsignaturaA,
+		//						equivalencias.nomAsignaturaA,
+		//						detalles.notaAsignaturaA,
+		//						detalles.notaAsignaturaB,
+		//						detalles.fechaActa,
+		//					}).ToList();
+		//		}
+		//	}
+		//	catch (Exception)
+		//	{
 
-				throw;
-			}
-		}
+		//		throw;
+		//	}
+		//}
+        //probando el linq
+        public object consultaReporte(int idAdecuacion)
+        {
+            try
+            {
+                using (adecuacionIscFiuplaEntities ctx = new adecuacionIscFiuplaEntities())
+                {
+                    return (from estudiante in ctx.estudiante
+                            join adecuacion in ctx.adecuacion on estudiante.idEstudiante equals adecuacion.idEstudiante
+                            join detalles in ctx.detalleAdecuacion on adecuacion.idAdecuacion equals detalles.idAdecuacion
+                            join equivalencias in ctx.equivalenciaAsignatura on detalles.idEquivalencia equals equivalencias.idEquivalencia                   
+                            where adecuacion.idAdecuacion == idAdecuacion
+                            select new
+                            {
+                                estudiante.apellidosEstudiante,
+                                estudiante.nombresEstudiante,
+                                estudiante.codEstudiante,
+                                estudiante.sede,
+                                estudiante.modalidad,
+                                estudiante.carrera,
+                                adecuacion.codigoAdecuacion,
+                                adecuacion.futAdecuacion,
+                                adecuacion.fechaRecepcion,
+                                adecuacion.periodo, 
+                                adecuacion.fechaEmision,
+                                equivalencias.planAsignaturaB,
+                                equivalencias.codAsignaturaB,
+                                equivalencias.nomAsignaturaB,
+                                equivalencias.codAsignaturaA,
+                                equivalencias.creditosAsignaturaA,
+                                equivalencias.cicloAsignaturaA,
+                                equivalencias.nomAsignaturaA,
+                                detalles.notaAsignaturaA,
+                                detalles.notaAsignaturaB,
+                                detalles.fechaActa,
+                            }).ToList();
+                }
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+
+        }
     }
 }
